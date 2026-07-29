1c58e6d2d7d0c54b8dbd6bec9e9288108fa31910
gatyxofc <gatyx.market@hotmail.com>
2026-07-29 23:51:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hello");
+
+const cursor = document.querySelector(".cursor")
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorX = 0;
+let cursorY = 0;
+
+document.addEventListener("mousemove", (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+function animateCursor() {
+    cursorX += (mouseX - cursorX) * 0.1;
+    cursorY += (mouseY - cursorY) * 0.1;
+
+    cursor.style.left = cursorX + "px";
+    cursor.style.top = cursorY + "px";
+
+    requestAnimationFrame(animateCursor);
+}
+
+animateCursor()
